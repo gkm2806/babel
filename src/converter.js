a567ce4293273f860db1fc487d8c7c542a0c1974
Gabriel K <gabrielkukiel@gmail.com>
2019-05-28 18:38:16 -0400

    Isolando os dados dentro do JS

--- src/converter.js
+++ src/converter.js
@@ -1,16 +1,31 @@
 const fs = require('fs');
-var parseString = require('xml2js').parseString;
+const parseString = require('xml2js').parseString;
 
-fs.readFile(__dirname+'/../assets/horario.xml', "utf-8", (err, xml) => {
+const xmltojson = async () => {
+await fs.readFile(__dirname + '/../assets/horario.xml', "utf-8", (err, xml) => {
     if (err) { console.log(err) }
     parseString(xml, (err, result) => {
+        result = result.Workbook.Worksheet[0].Table[0]
         var json = JSON.stringify(result)
-        fs.appendFile(__dirname+'/../assets/horarios.json', json, (err) => {
+        fs.appendFile(__dirname + '/../assets/horarios.json', JSON.stringify(result), (err) => {
             if (err) throw err;
-            console.log(result.Workbook.Worksheet[0].Table[0].Row[1].Cell[9]);
+            var aulas = []
+
+            for(var i = 0; i < result.Row.length; i++){
+                for(var j = 0; j < result.Row[i].Cell.length; j++){
+                    var newAula = {}
+                    /*
+                        [4] 
+                    */
+                    console.log(result.Row[i].Cell[j].Data[0]._)
+                }
+            }
+            
         });
     });
-}) 
+})
+}
+xmltojson()
 
 
 
