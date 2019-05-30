0dd54f8f7f27d896073dd2304cee045d61d0fc0c
Gabriel K <gabrielkukiel@gmail.com>
2019-05-30 08:10:05 -0400

    adicionando vetores auxiliares para dia e hora.

--- src/converter.js
+++ src/converter.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const parseString = require('xml2js').parseString;
 
+const dias2dia = ['segunda','terça','quarta','quinta','sexta','sabado']
+const periodo2hora = [[],['07:00','07:45'],['7:45','8:30'],['08:30','9:15'],['9:30','10:15'],['10:15','11:00'],['11:00','11:45'],['11:45','12:30'],['13:00','13:45'],['13:45','14:30'],['14:30','15:15'],['15:30','16:15'],['16:15','17:00'],['17:00','17:45'],['17:45','18:30'],['19:00','19:45'],['19:45','20:30'],['20:30','21:15'],['21:30','22:15'],['22:15','23:00']]
+
 const xmltojson = async () => {
     await fs.readFile(__dirname + '/../assets/horario.xml', "utf-8", (err, xml) => {
         if (err) { console.log(err) }
@@ -10,22 +13,27 @@ const xmltojson = async () => {
             var aulas = []
 
             for (var i = 0; i < result.Row.length; i++) {
+
+
                 try {
                     var newAula = {
                         "sigla": (result.Row[i].Cell[3].Data[0]._).toString(),
                         "turma": (result.Row[i].Cell[4].Data[0]._).toString(),
                         "materia": (result.Row[i].Cell[5].Data[0]._).toString(),
-                        "periodo": (result.Row[i].Cell[6].Data[0]._).toString(),
-                        "dias": (result.Row[i].Cell[7].Data[0]._).toString(),
+                        "horaInicio": periodo2hora[(result.Row[i].Cell[6].Data[0]._).toString()][0],
+                        "horaFim": periodo2hora[(result.Row[i].Cell[6].Data[0]._).toString()][1],
                         "professor": (result.Row[i].Cell[9].Data[0]._).toString(),
-                        "sala": (result.Row[i].Cell[10].Data[0]._).toString(),
+                        "sala": (result.Row[i].Cell[10].Data[0]._).toString()
                     }
+
                     aulas.push(newAula)
                 } catch (e) {
-                    console.log("Olou")
+                    console.log("Erro: ", e)
                 } finally {
                     console.log("vamo que vamo")
                 }
+                
+                
             }
             console.log(JSON.stringify(aulas))
 
